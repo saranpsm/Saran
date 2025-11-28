d0114c984dfae717f2a09c4911ec6d1c03a95871
Saran K <131590642+saranpsm@users.noreply.github.com>
2025-11-28 23:42:22 +0530

    Create Program-2.java

---
+++ Program-2.java
@@ -0,0 +1,16 @@
+class Sample {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+     System.out.println("Enter the iteration:");
+     int n=sc.nextInt();
+      int j=1;
+       
+        for (int i=1;i<=n;i++)
+        {  
+            System.out.print(j+",");
+            j+=2;
+        }
+        sc.close();
+    }
+}

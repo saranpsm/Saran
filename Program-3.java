e9429473872bc206eaa8ac80ccedf1c4e1044d7e
Saran K <131590642+saranpsm@users.noreply.github.com>
2025-11-28 23:46:23 +0530

    Create Program-3.java

---
+++ Program-3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Sample {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+     System.out.println("Enter the iteration:");
+     int n=sc.nextInt();
+      int j=1;
+       if(n%2==0)
+       {
+         n=n-1;
+       }
+        for (int i=1;i<=n;i++)
+        {  
+            System.out.print(j+",");
+            j+=2;
+        }
+        sc.close();
+    }
